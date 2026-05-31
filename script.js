d54daddee2015b154f0805912b5ae7ff87a8f170
alexis <alexismejor10@hotmail.es>
2026-05-31 17:12:43 -0500

    Set Predicador as latest release

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const latestRelease = {
   title: "Predicador",
   trackingTitle: "Predicador",
   slug: "predicador",
-  artist: 'ZAETTA',
+  artist: "ZAETTA ft. Alex Rodriguez & Julian Ramos",
   cover: "assets/predicador-cover.jpg",
   link: "https://open.spotify.com/intl-es/track/5C52EVyuczyiEvYGr4wDK5",
   browserTitle: "ZAETTA - Escucha Predicador",
