7b1e35d93a6707410b6e39d3bcaf964a6741959b
AlexisAlzate <39492599+alexis-alzate@users.noreply.github.com>
2026-06-05 18:09:59 -0500

    Set Predicador as latest release

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const latestRelease = {
-  title: "Gloria y Honra",
-  trackingTitle: "Gloria y Honra",
-  slug: "gloriayhonra",
+  title: "Predicador",
+  trackingTitle: "Predicador",
+  slug: "predicador",
   artist: 'ZAETTA',
-  cover: "assets/gloriayhonra-cover.jpg",
-  link: "https://too.fm/dewo8xz",
-  shareUrl: "https://www.lujourban.com/lanzamientos/gloriayhonra-v2/",
-  browserTitle: "ZAETTA - Escucha Gloria y Honra",
+  cover: "assets/predicador-cover.jpg",
+  link: "https://open.spotify.com/track/5C52EVyuczyiEvYGr4wDK5?si=tXpu1_WGRriQA3E8-DvPWA",
+  shareUrl: "https://www.lujourban.com/lanzamientos/predicador-v4/",
+  browserTitle: "ZAETTA - Escucha Predicador",
   heroText: "Música con propósito. Sonidos que trascienden."
 };
 
